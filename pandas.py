688607056286717493c2ab1c7fd2592c0c4e467a
Humberto Della Torre <humbertodlltrr@gmail.com>
2019-01-22 19:55:43 +0100

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -15,11 +15,9 @@ col = ["Sex","NOC","Year","Sport"]
 
 for i in range(len(col)):
     df["Height"].fillna(round(df.groupby(col[:len(col)-i])["Height"].transform("mean")), inplace=True)
-df["Height"].fillna(round(df["Height"].mean()), inplace=True)
 
 for i in range(len(col)):
     df["Weight"].fillna(round(df.groupby(col[:len(col)-i])["Weight"].transform("mean")), inplace=True)
-df["Weight"].fillna(round(df["Weight"].mean()), inplace=True)
 
 print("After:\n",df.isnull().sum()/len(df)*100)
 #df.to_csv("example.csv")
